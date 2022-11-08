c29ac5cf8502450db9bf2a1788c43ae6f3a72381
aid946@gmail.com <aid946@gmail.com>
2022-11-08 23:02:09 +0500

    Telegram bot

--- markups.py
+++ markups.py
@@ -1,10 +1,12 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config as cfg
 
+# кнопка для перехода в канал:
 btnUrlChannel = InlineKeyboardButton(text="Канал", url=cfg.CHANNEL_URL)
 channelMenu = InlineKeyboardMarkup(row_width=1)
 channelMenu.insert(btnUrlChannel)
 
+# кнопка для перехода к боту:
 botUrl = InlineKeyboardButton(text="Перейти к боту", url=cfg.BOT_URL)
 botMenu = InlineKeyboardMarkup(row_width=1)
 botMenu.insert(botUrl)
